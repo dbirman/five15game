f2f44b6768c9074e565c02171a6479114403673e
Dan Birman <danbirman@gmail.com>
2020-05-05 21:50:48 -0700

    Adding a countdown

--- code.js
+++ code.js
@@ -13,6 +13,40 @@ c.height = window.innerHeight;
 var ctx=c.getContext("2d");
 ctx.fillStyle="#ffffff";
 ctx.strokeStyle="#ffffff";
+
+
+//countdown
+// Set the date we're counting down to
+var countDownDate = new Date("May 18, 2020 12:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "WE LAUNCHED!";
+  }
+}, 1000);
+
+
+
 //Twinkle
 var curTwinkle = -1;
 
